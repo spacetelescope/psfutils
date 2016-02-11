0beae56fb51fa814ba0287d0ed387a1c84cff686
Larry Bradley <larry.bradley@gmail.com>
2016-02-11 13:52:59 -0500

    Add other 1D centroid methods

--- psfutils/centroid.py
+++ psfutils/centroid.py
@@ -10,7 +10,8 @@ import numpy as np
 __all__ = ['local_centroid']
 
 
-def _local_centroid1d(data):
+def _jay_local_centroid1d(data):
+    data = np.asanyarray(data)
     numer = (data[2] - data[0]) / 2.
     denom = data[1] - np.min([data[0], data[2]])
     if denom == 0:
@@ -19,15 +20,30 @@ def _local_centroid1d(data):
         return (numer / denom)
 
 
-def local_centroid(data, max_position=None):
+def _1d_local_centroid1d(data, submin=False):
+    data = np.asanyarray(data)
+    if submin:
+        data = data - data.min()
+    return (data[2] - data[0]) / data.sum()
+
+
+def local_centroid(data, mode='jay', max_position=None):
     """
-    Calculate the local centroid of a 2D array using Jay Anderson's
-    method.
+    Calculate the local centroid of a 2D array using the 1D marginal x
+    and y centroids of the central three pixels.
 
     Parameters
     ----------
     data : array_like
         The 2D array of the image.
+
+    mode : {'jay', '1d', '1dsub'}, optional
+        The centroid algorithm to use:
+
+            * ``'jay'``: Jay Anderson's algorithm (default).
+            * ``'1d'``: Standard 1D moment.
+            * ``'1dsubmin'``: Standard 1D moment after subtraction of
+              the minimum pixel value.
 
     max_position : 2-tuple of float, optional
         The (y, x) position of the maximum pixel in the 2D array.  If
@@ -49,5 +65,15 @@ def local_centroid(data, max_position=None):
     if len(xdata) != 3 or len(ydata) != 3:
         raise ValueError('max_position cannot be on the data edge')
 
-    return (_local_centroid1d(xdata) + xmax,
-            _local_centroid1d(ydata) + ymax)
+    if mode == 'jay':
+        xc = _jay_local_centroid1d(xdata)
+        yc = _jay_local_centroid1d(ydata)
+    elif mode == '1d':
+        xc = _1d_local_centroid1d(xdata, submin=False)
+        yc = _1d_local_centroid1d(ydata, submin=False)
+
+    elif mode == '1dsubmin':
+        xc = _1d_local_centroid1d(xdata, submin=True)
+        yc = _1d_local_centroid1d(ydata, submin=True)
+
+    return (xc + xmax, yc + ymax)
